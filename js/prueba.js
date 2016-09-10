9cdf1444c57807aba7b9716742fed860dfba1ad9
duard1384 <duard.bol@gmail.com>
2016-09-11 01:12:09 +0200

    desarrollo index

---
+++ js/prueba.js
@@ -0,0 +1,9 @@
+ window.onload = function what() {
+     document.getElementById("demo").innerHTML = "My First Page";
+     document.getElementsByClassName("portada").innerHTML = "Date();";
+ }
+ what();
+
+ function que() {
+     document.getElementById("que").innerHTML = "My First click";
+ }
