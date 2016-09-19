f0a9764071bc21c3781f0c9510363c173bb78176
duard1384 <duard.bol@gmail.com>
2016-09-19 05:56:47 +0200

    restauracion all

--- js/prueba.js
+++ js/prueba.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 /*
-=======
->>>>>>> 9cdf1444c57807aba7b9716742fed860dfba1ad9
  window.onload = function what() {
      document.getElementById("demo").innerHTML = "My First Page";
      document.getElementsByClassName("portada").innerHTML = "Date();";
@@ -10,9 +7,5 @@
 
  function que() {
      document.getElementById("que").innerHTML = "My First click";
-<<<<<<< HEAD
  }
  */
-=======
- }
->>>>>>> 9cdf1444c57807aba7b9716742fed860dfba1ad9
