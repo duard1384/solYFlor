9c993b647325dc55feeef2db1ec5a206ab98fd0a
duard1384 <duard.bol@gmail.com>
2016-09-19 05:13:11 +0200

    terminado index

---
+++ js/prueba.js
@@ -0,0 +1,11 @@
+/*
+ window.onload = function what() {
+     document.getElementById("demo").innerHTML = "My First Page";
+     document.getElementsByClassName("portada").innerHTML = "Date();";
+ }
+ what();
+
+ function que() {
+     document.getElementById("que").innerHTML = "My First click";
+ }
+ */
